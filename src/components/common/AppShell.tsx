5ff23d5511adb2f3a40c316c0b9466ae2eec38f0
성태경 <taegyeong0320@naver.com>
2026-05-08 14:54:16 +0900

    feat: AppShell에 사이드바 영역 CSS 변수 추가

--- src/components/common/AppShell.tsx
+++ src/components/common/AppShell.tsx
@@ -9,13 +9,20 @@ export function AppShell({ children }: { children: React.ReactNode }) {
   const [collapsed] = React.useState(false);
 
   return (
-    <>
+    <div
+      className={cn(
+        // sidebar-width는 사이드바 자체 너비, content-left는 left-4 여백까지 포함한 콘텐츠 시작 위치
+        collapsed
+          ? '[--app-content-left:89px] [--app-sidebar-width:73px]'
+          : '[--app-content-left:268px] [--app-sidebar-width:252px]',
+      )}
+    >
       <Sidebar collapsed={collapsed} />
       <main
         className={cn('min-h-[calc(100vh-32px)] min-w-0', collapsed ? 'ml-[73px]' : 'ml-[252px]')}
       >
         {children}
       </main>
-    </>
+    </div>
   );
 }
