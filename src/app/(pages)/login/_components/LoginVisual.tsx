03388e338ec33cea16f26ad76bb8e90e7cec6913
psm1st <psm1st@sookmyung.ac.kr>
2026-05-27 03:35:38 +0900

    fix: 컴포넌트를 로그인 폴더로 이동 후 디자인 수정

--- src/app/(pages)/login/_components/LoginVisual.tsx
+++ src/app/(pages)/login/_components/LoginVisual.tsx
@@ -2,7 +2,7 @@ import { LottieAnimation } from '@/components/common/LottieAnimation';
 
 export function LoginVisual() {
   return (
-    <div className="hidden h-screen min-h-[720px] flex-1 overflow-hidden bg-gray-main lg:block">
+    <div className="hidden h-screen min-h-[720px] flex-1 overflow-hidden bg-[#FAFAFA] lg:block">
       <LottieAnimation
         src="/lotties/login.lottie"
         ariaLabel="끼움 서비스 소개 애니메이션"
