8b1b623d06c903770687d3a719b47853d396e50a
성태경 <taegyeong0320@naver.com>
2026-05-27 02:49:12 +0900

    feat: 로그인 Lottie 비주얼 추가

---
+++ src/components/common/LoginVisual.tsx
@@ -0,0 +1,13 @@
+import { LottieAnimation } from '@/components/common/LottieAnimation';
+
+export function LoginVisual() {
+  return (
+    <div className="hidden h-screen min-h-[720px] flex-1 overflow-hidden bg-gray-main lg:block">
+      <LottieAnimation
+        src="/lotties/login.lottie"
+        ariaLabel="끼움 서비스 소개 애니메이션"
+        className="h-full w-full object-cover"
+      />
+    </div>
+  );
+}
