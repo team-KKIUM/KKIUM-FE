3ac41624040b280c08d3209baa6c9282182d1419
psm1st <psm1st@sookmyung.ac.kr>
2026-05-27 03:33:19 +0900

    feat: 카카오 로그인 구현

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import { GoogleLoginButton } from './_components/GoogleLoginButton';
+import { KakaoLoginButton } from './_components/KakaoLoginButton';
+import { LoginVisual } from './_components/LoginVisual';
+
+export default function LoginPage() {
+  return (
+    <main className="flex min-h-screen w-full items-stretch bg-white">
+      <div className="flex w-full items-center justify-center bg-white p-6 lg:w-[668px] lg:min-w-[668px]">
+        <section className="inline-flex w-full flex-col items-center gap-12 overflow-hidden rounded-xl bg-white px-8 py-12 sm:px-16 sm:py-20 lg:w-[668px]">
+          <div className="flex flex-col items-center gap-5">
+            <Image src="/logo-big.svg" alt="KKIUM 로고" width={258} height={49} priority />
+            <p className="body-1-bold text-center text-primary">
+              경험을 기록하고 분석하여
+              <br />
+              나만의 스토리를 완성하세요
+            </p>
+          </div>
+
+          <div className="flex w-full max-w-72 flex-col items-start gap-3">
+            <KakaoLoginButton />
+            <GoogleLoginButton />
+          </div>
+        </section>
+      </div>
+      <LoginVisual />
+    </main>
+  );
+}
