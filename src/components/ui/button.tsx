b4ebd4fcedb525d2c94af9394fe2c95885eb67d0
성태경 <taegyeong0320@naver.com>
2026-05-28 00:22:17 +0900

    feat: 404 페이지 구현

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { Slot } from '@radix-ui/react-slot';
+import { Slot } from 'radix-ui';
 
 import { cn } from '@/lib/utils';
 
@@ -52,19 +52,34 @@ function Button({
   asChild = false,
   ...props
 }: ButtonProps) {
-  const Comp = asChild ? Slot : 'button';
+  const Comp = asChild ? Slot.Root : 'button';
+  const buttonClassName = cn(buttonVariants({ variant, size, className }));
+
+  if (asChild) {
+    return (
+      <Comp
+        data-slot="button"
+        data-variant={variant}
+        data-size={size}
+        className={buttonClassName}
+        {...props}
+      >
+        {children}
+      </Comp>
+    );
+  }
 
   return (
     <Comp
       data-slot="button"
       data-variant={variant}
       data-size={size}
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={buttonClassName}
       {...props}
     >
-      {!asChild && leftIcon && <ButtonIcon size={size}>{leftIcon}</ButtonIcon>}
+      {leftIcon && <ButtonIcon size={size}>{leftIcon}</ButtonIcon>}
       {children}
-      {!asChild && rightIcon && <ButtonIcon size={size}>{rightIcon}</ButtonIcon>}
+      {rightIcon && <ButtonIcon size={size}>{rightIcon}</ButtonIcon>}
     </Comp>
   );
 }
