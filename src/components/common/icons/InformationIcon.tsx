9df199e71dc3718a12b304f8519714e120217b50
psm1st <psm1st@sookmyung.ac.kr>
2026-05-29 04:24:27 +0900

    fix: information icon 수정

--- src/components/common/icons/InformationIcon.tsx
+++ src/components/common/icons/InformationIcon.tsx
@@ -2,10 +2,27 @@ import type { SVGProps } from 'react';
 
 export function InformationIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
-      <circle cx="10" cy="10" r="8.17" stroke="currentColor" strokeWidth="1.5" />
-      <circle cx="10" cy="7.33" r="1" fill="currentColor" />
-      <rect x="9.17" y="10" width="1.67" height="6.5" rx="0.83" fill="currentColor" />
+    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" aria-hidden="true" {...props}>
+      <path
+        d="M18.333 10.9997C18.333 6.94959 15.0498 3.66634 10.9997 3.66634C6.94959 3.66634 3.66634 6.94959 3.66634 10.9997C3.66634 15.0498 6.94959 18.333 10.9997 18.333L10.9997 20.1663C5.93707 20.1663 1.83301 16.0623 1.83301 10.9997C1.83301 5.93706 5.93707 1.83301 10.9997 1.83301C16.0623 1.83301 20.1663 5.93707 20.1663 10.9997C20.1663 16.0623 16.0623 20.1663 10.9997 20.1663L10.9997 18.333C15.0498 18.333 18.333 15.0498 18.333 10.9997Z"
+        fill="currentColor"
+      />
+      <rect
+        x="11.917"
+        y="7.33301"
+        width="1.83333"
+        height="1.83333"
+        transform="rotate(-180 11.917 7.33301)"
+        fill="currentColor"
+      />
+      <rect
+        x="11.917"
+        y="16.5"
+        width="1.83333"
+        height="7.33333"
+        transform="rotate(-180 11.917 16.5)"
+        fill="currentColor"
+      />
     </svg>
   );
 }
