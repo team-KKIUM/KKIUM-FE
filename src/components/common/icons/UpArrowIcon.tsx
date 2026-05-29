d18b8873ce81329edfe02e0510d97c860d552b93
psm1st <psm1st@sookmyung.ac.kr>
2026-05-30 00:48:53 +0900

    feat: 필요한 아이콘 추가

---
+++ src/components/common/icons/UpArrowIcon.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from 'react';
+
+export function UpArrowIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {...props}>
+      <path
+        d="M18.9885 5.0083V16.5002L16.9881 16.4995V8.42299L6.00097 19.4101L4.58676 17.9959L15.5725 7.01016L7.50081 7.01361L7.49943 5.01383L18.9885 5.0083Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
